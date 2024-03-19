49725a7389472f8f5aac6f319c0e3d9f94131857
Hoang Anh <hoanganhlclqd@gmail.com>
2024-03-20 01:25:38 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import IconsGroup from "./IconsGroup";
 
 const Footer = () => {
   return (
-    <div className="w-screen items-end bg-transparent">
+    <div className="w-screen items-end bg-transparent pb-2">
       <div className="flex justify-center w-full py-2 h-full items-end">
         <p className="text-advertisement-button">© </p>
         <p className="px-1 font-bold text-advertisement-button"> 2024 Highlands Coffee. </p>
